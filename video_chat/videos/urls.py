31632bed336251f6f5bd372d771c01dcee3ea133
Juan Bollati <juanbollati@gmail.com>
2016-10-17 22:49:39 -0300

    Implemento view search

---
+++ video_chat/videos/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'videos'
+urlpatterns = [
+    url(r'^search/(?P<search_key>[\w\-]+)/$', views.SearchView.as_view(), name='search'),
+    #url(r'^(?P<video_id>[0-9]+)/delete/$', views.delete, name='delete'),
+]
--- video_chat/videos/views.py
+++ video_chat/videos/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Video
 
 # Create your views here.
+class SearchView(generic.ListView):
+    template_name = 'videos/search.html'
+    context_object_name = 'founded_videos'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Video.objects.filter(title__contains=self.kwargs['search_key'])
