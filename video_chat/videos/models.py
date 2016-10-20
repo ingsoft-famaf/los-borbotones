afe39fd96e6c4cc8bcb4696793ff0c774e987777
Alejandro Nigrelli <alejandro.nigrelli@gmail.com>
2016-10-19 21:32:45 -0300

    Subo validacion de tipo de archivo y generalizacion de ruta para la reproduccion de video

--- video_chat/videos/models.py
+++ video_chat/videos/models.py
@@ -1,13 +1,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from .validators import validate_file_extension
 # Create your models here.
 class Video(models.Model):
     title = models.CharField(max_length = 30)
     description = models.CharField(max_length = 300)
     pub_date = models.DateTimeField('date_published', auto_now_add=True)
-    file = models.FileField(upload_to='videos/%Y/%m/%d/')
+    file = models.FileField(upload_to='videos/%Y/%m/%d/',validators = [validate_file_extension])
     #autor = models.ForeignKey('User', on_delete=models.SET_DEFAULT, default="None")
 
     def __str__(self):
