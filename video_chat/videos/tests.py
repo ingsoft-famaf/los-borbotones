18e0d14c36e2fad17a0e9ee0997d00de80c331c1
Alejandro Nigrelli <alejandro.nigrelli@gmail.com>
2016-10-23 20:23:29 -0300

    Agrego algunos test sencillos

--- video_chat/videos/tests.py
+++ video_chat/videos/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test import Client
+from .models import Video
 
-# Create your tests here.
+class view_tests(TestCase):
+	def test_view_error_404_random_string(self):
+		client = Client()
+		response = client.get('/alkfddaklfjh')
+		self.assertEqual(response.status_code, 404)
+
+	def test_view_code_200(self):
+		client = Client()
+		response = client.get('/')
+		self.assertEqual(response.status_code, 200)
