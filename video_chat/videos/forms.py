1dba46d444e8f62afe838946f4f1210bc5652e59
Juan Bollati <juanbollati@gmail.com>
2016-10-28 14:41:11 -0300

    - Cambio view upload por class-based view "CreateView" - Modifico template upload, agrego bootstrap. - Unifico idioma. - Modifico errores template delete_video

--- video_chat/videos/forms.py
+++ video_chat/videos/forms.py
@@ -3,12 +3,18 @@ from django import forms
 from .models import Video
 
 
-class VideoForm(forms.ModelForm):
+class UploadForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(UploadForm, self).__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs = {
+            'class': 'form-control'
+        }
+        self.fields['description'].widget.attrs = {
+            'class': 'form-control'
+        }
+
+
     class Meta:
         model = Video
         fields = ('title','description', 'file', )
 
-	class Media:
-		css = {
-			'all': ('style.css',)
-		}
